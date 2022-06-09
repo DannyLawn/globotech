cef01e02ac37663c7642a2f2cff2ca56298ba56f
Daniil Nikitin <danny.lawn@yandex.ru>
2022-06-10 00:59:50 +0300

    Созданы разделы сайта, улучшена адаптивность

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+const productsContainer = document.querySelector('.products__container');
+
+function createProduct(object) {
+  const productTemplate = productsContainer.querySelector('.product-template').content;
+  const productItem = productTemplate.querySelector('.products__item').cloneNode(true);
+  productItem.querySelector('.products__image').src = object.image;
+  productItem.querySelector('.products__image').alt = object.name;
+  productItem.querySelector('.products__title').textContent = object.name;
+  productItem.querySelector('.products__text').textContent = object.description;
+  return productItem;
+}
+
+function addProduct (newProduct) {
+  productsContainer.append(newProduct);
+}
+
+
+initialProducts.forEach(product => {
+  addProduct(createProduct(product));
+});
+
+
---
+++ scripts/products.js
@@ -0,0 +1,17 @@
+const initialProducts = [
+  {
+    name: 'Development',
+    description: 'Development, refinement, programming, documentation, testing and error correction. We are distinguished by the ability to solve problems and not be afraid of difficulties. Our activity is the constant creation of a new one.',
+    image: './images/products/develop.jpg'
+  },
+  {
+    name: 'Design',
+    description: 'Not only stunning visual design, but most importantly user convenience. This will be a great advantage over competitors.',
+    image: './images/products/design.jpg'
+  },
+  {
+    name: 'Support',
+    description: 'We will set up analytics on the site and conduct advertising, and subsequently modify and maintain the site content. This way the product will be able to work for the benefit of the business.',
+    image: './images/products/support.jpg'
+  }
+  ];
